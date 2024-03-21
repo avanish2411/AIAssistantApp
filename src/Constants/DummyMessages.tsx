e5e32b5fc689e3eb7f00b05e195be749d190a932
avanish2411 <avanishsingh2411@gmail.com>
2024-03-21 23:41:02 +0530

    pre-final commit with ui

---
+++ src/Constants/DummyMessages.tsx
@@ -0,0 +1,19 @@
+export const DummyMessage = [
+    {
+        role :'user',
+        content: 'How are you?'
+    },
+    {
+        role :'assistant',
+        content: 'I am fine,How may i help to today?'
+    },
+    {
+        role :'user',
+        content: 'create an imnage of a dog playing with cat'
+    },
+    {
+        role :'assistant',
+        content: 'https://www.pinterest.com/pin/9992430395996755/'
+    },
+
+]
